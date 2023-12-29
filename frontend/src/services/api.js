8e62e6d6a362d9b505f97330e3a238327f7844d2
santitassara <86531696+santitassara@users.noreply.github.com>
2023-12-29 19:07:17 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', 
+  baseURL: 'https://notes-app-api1.onrender.com//api', 
 });
 
 export const createNote = async (data) => {
